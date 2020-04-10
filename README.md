663d4500e166350360f5932821511ab25df9cdf5
Gqiangqiang <1909809427@qq.com>
2020-04-10 15:23:52 +0800

    add tow methods

--- README.md
+++ README.md
@@ -4,42 +4,53 @@
     Make you more free to get time and combine it into the format you want
 
 # 使用方法(How to use)
-    现在的默认格式为(default format)：
-        var fd = require('freedate')
-        var date = fd();
-        console.log(date); //2020/01/04 15:21:37
-
+```js
+现在的默认格式为(default format)：
+    var fd = require('freedate');
+    var date = fd.getTime();
+    console.log(date); //2020/01/04 15:21:37
+```
 ## 更多(more)
-    如(for example)：
-        fd("Y-M-D") ===> 2020-1-4
+```js 
+如(for example)：
+    fd.getTime("Y-M-D") ==> '2020-1-4'
 
-        fd("M DD") ===> 1 04
+    fd.getTime("M DD") ==> '1 04'
 
-        fd("h:m:s") ===> 3:23:33
-        
-        fd("Y/M/D h:m:s") ==> 2020/1/4 3:23:33
+    fd.getTime("h:m:s") ==> '3:23:33'
+    
+    fd.getTime("Y/M/D h:m:s") ==> '2020/1/4 3:23:33'
 
-        fd("Y/MM/DD W hh:mm:ss") ==> 2020/01/04 Thur 15:23:33
+    fd.getTime("Y/MM/DD W hh:mm:ss") ==> '2020/01/04 Thur 15:23:33'
 
-        fd("Y/MM/DD w hh:mm:ss") ==> 2020/01/04 星期四 15:23:33
+    fd.getTime("Y/MM/DD w hh:mm:ss") ==> '2020/01/04 星期四 15:23:33'
 
-        fd("Y年 MM月 DD日 w hh:mm:ss") ==> 2020年 01月 04日 星期四 15:23:33
+    fd.getTime("Y年 MM月 DD日 w hh:mm:ss") ==> '2020年 01月 04日 星期四 15:23:33'
 
-        fd("现在是：Y年MM月DD日 w 下午 hh点mm分") ==> 现在是：2020年01月04日 星期四 下午 15点23分  
+    fd.getTime("现在是：Y年MM月DD日 w 下午 hh点mm分") ==> '现在是：2020年01月04日 星期四 下午 15点23分'
 
-        fd("MM/DD/Y hh:mm:ss") ==> 1/4/2020 03:23:33
-
-        fd("TS") ==> 1578121532 
-
+    fd.getTime("MM/DD/Y hh:mm:ss") ==> '1/4/2020 03:23:33'
+    
+    //把已有的其他时间格式化
+    fd.getTime("Y/MM/DD hh:mm:ss W",fd.tsToDate(fd.getTS(10))) ==> "2020/04/20 14:43:05 Mon" //'otherDate' must be type of Date
+    // get the timestamp
+    fd.getTS() ==> 1586491413246   //现在 Now
+    fd.getTS(-1) ==> 1586405054923 //一天前 A day earlier
+    fd.getTS(1) ==> 1586577868836 //一天后 After a day
+    
+    // timestamp to date
+    fd.tsToDate(1586491413246) ==> '2020-04-10T04:03:33.246Z'
+    fd.tsToDate(1586491413246,"Y/MM/DD hh:mm:ss W") ==> '2020/04/10 12:03:33 Fri'
+```
     如果有两个“D”，那么当数字小于10的时候，有零在这个数字前面，就像“01”或者“02”;如果只有一个“D”的话，就没有这个0。使用过程中可以混合使用。
 
     If there are two "D", then when the number is less than 10, there is zero in front of the number, such as "01" or "02"; if there is only one "D", there is no zero. Can be mixed during use.
 
 
-# 代码对照表(Comparison table)
+# getTime参数对照表(Comparison table)
 | code | return | for example |
 |:-:|:-:|:-:|
-|  Y  |  返回年(`year`) | 如2020
+|  Y  |  返回年(`year`) | 2018,2019,2020...
 |  M  |  返回月(`month`)，没有前置0 | 1~12
 |  MM |  返回月(`month`)，有前置的0 | 01~12
 |  D  |  返回日(`day`)，没有前置0 | 1~31
@@ -54,24 +65,45 @@
 |  W  |  返回星期(`week` English)， | Sun/Mon/Tue/Wed/Thur/Fri/Sat
 |  TS | 返回时间戳(`time stamp`) | 1578121532
 
-## 注意(Attention)
-**`When word splicing is needed, JS is not very friendly to English users`**
+# 方法（methods）
+- **getTime(timestr, newDate)**
+    - `timestr` 类型必须是`String`，可以为空(The type must be a `string` and ***can be null***)  
+    - `newDate` 类型必须是`Date`，可以为空(The type must be a `Date` and ***can be null***)  
 
-`fd`函数中会把在字符串中出现的第一个符合条件的字符替换为时间，且再次出现的字符不会再被替换。  
+- **getTS(offset)**  
+    -  `offset` 必须是`number`类型,可以为空,可以为负值（负值就是几天前，正值就是几天后），如果为空获取的是当前时间的时间戳,(The type must be a `number` and ***can be null***. It can be negative if it is null to get the timestamp of the current time)  
+
+- **tsToDate(ts,datestr)**
+    - `ts` 必须是`number`类型，不能为空(The type must be a `number` and ***cannot be null***)
+    - `datestr` 必须是`String`类型，可以为空(The type must be a `string` and ***can be null***)
+
+## 注意(Attention)
+**`When word splicing is needed, freedate is not very friendly to English users`**
+
+`fd`函数中会把在字符串中出现的第一个符合条件的字符替换为时间，且再次出现的字符不会再被替换。
 ```js
-    fd("M M M") ==> 4 M M。
+    fd.getTime("M M M") ==> 4 M M。
 ```
 以M为例，MM的优先级比M要高，所以如果输入的字符串中有M也有MM那么被替换的会是MM。
 ```js
-    fd("M-MM-MM") ==> M-04-MM。
+    fd.getTime("M-MM-MM") ==> M-04-MM。
 ```
 The `fd` function will replace the first eligible character in the string with the time, and the character that appears again will not be replaced.
 ```js
-    fd("M M M") ==> 4 M M。
+    fd.getTime("M M M") ==> 4 M M。
 ``` 
 For example, the priority of MM is higher than that of M, so if there is also MM in the input string, MM will be replaced.
 ```js
-    fd("M-MM-MM") ==> M-04-MM。
+    fd.getTime("M-MM-MM") ==> M-04-MM。
 ```
 
-*Use Baidu translation (there may be inaccuracies)*
+# 版本(Version)
+***`If necessary, please translate by yourself,thanks for your understanding.`***
+
+**V1.0.2** 
+- 修改了时间的获取方式：引入后通过`fd.getTime()`的方式来使用方法
+- 增加`getTS()`来获取时间戳,移除了通过`getTime("TS")`来获取时间戳的方式，可以输入参数“天”
+- 增加`tsToDate()`来把`timestamp`转换为`Date`
+
+**V1.0.1**
+- 增加了使用字符串"TS"来获取时间戳。(Added the use of the string "TS" to get the timestamp.)
--- freedate.js
+++ freedate.js
@@ -1,110 +1,165 @@
-/*	如： Y/M/D h:m:s 		==> 2020/1/4 03:23:33
-		 Y/MM/DD w hh:mm:ss ==> 2020/01/04 星期四 15:23:33
-		 Y/MM/DD 			==> 2020/01/04
-*/
-function fd(timestr){
-	var datestr = timestr;			//赋值给内部变量以便进行操作
-	var fdate = new Date();			//新建Date对象
-	var TS = fdate.valueOf();		//获取时间戳
-	var Y = fdate.getFullYear();	//获取年
-	var M =fdate.getMonth() + 1;	//获取月 从0开始，所以得到的数值加1
-	var D = fdate.getDate();		//获取日
-	var h = fdate.getHours();		//获取小时
-	var m = fdate.getMinutes();		//获取分
-	var s = fdate.getSeconds();		//获取秒
-	var w = fdate.getDay();			//获取星期
-	var W;
-	switch (w) {
-		case 1:
-			w = '星期一';
-			W = 'Mon';
-			break;
-		case 2:
-			w = '星期二';
-			W = 'Tue';
-			break;
-		case 3:
-			w = '星期三';
-			W = 'Wed';
-			break;
-		case 4:
-			w = '星期四';
-			W = 'Thur';
-			break;
-		case 5:
-			w = '星期五';
-			W = 'Fri';
-			break;
-		case 6:
-			w = '星期六';
-			W = 'Sat';
-			break;
-		case 0:
-			w = '星期日';
-			W = 'Sun';
-			break;
-		default:
-			w = '星期获取出错';
-			W = 'Error';
-			break;
-	}
-	if(datestr == "" || datestr == undefined){
-		datestr = "Y/MM/DD hh:mm:ss";
-	}
-	if(datestr.indexOf("Y") != -1){
-		datestr = datestr.replace("Y", Y);
-	}
-	if(datestr.indexOf("M") != -1){
-		if(datestr.indexOf("MM") != -1){
-			datestr = datestr.replace("MM", addZero(M));
+var fdate = new Date();			//新建Date全局变量
+var fd = {
+	/**
+	 * get formatting time
+	 * @param {string} timestr 
+	 * @param {Date} newDate 
+	 */
+	getTime:function(timestr,newDate){
+		if(typeof(timestr) !== "string"){
+			throw new Error("The first parameter must be of type 'string'");
+		}
+		if(newDate !== undefined){
+			if(newDate instanceof Date){
+				fdate = newDate;
+			} else {
+				throw new Error("The second parameter must be of type 'Date'");
+			}
+		}
+		var datestr = timestr;			//赋值给内部变量以便进行操作
+		var Y = fdate.getFullYear();	//获取年
+		var M = fdate.getMonth() + 1;	//获取月 从0开始，所以得到的数值加1
+		var D = fdate.getDate();		//获取日
+		var h = fdate.getHours();		//获取小时
+		var m = fdate.getMinutes();		//获取分
+		var s = fdate.getSeconds();		//获取秒
+		var w = fdate.getDay();			//获取星期
+		var W;
+		switch (w) {
+			case 1:
+				w = '星期一';
+				W = 'Mon';
+				break;
+			case 2:
+				w = '星期二';
+				W = 'Tue';
+				break;
+			case 3:
+				w = '星期三';
+				W = 'Wed';
+				break;
+			case 4:
+				w = '星期四';
+				W = 'Thur';
+				break;
+			case 5:
+				w = '星期五';
+				W = 'Fri';
+				break;
+			case 6:
+				w = '星期六';
+				W = 'Sat';
+				break;
+			case 0:
+				w = '星期日';
+				W = 'Sun';
+				break;
+			default:
+				w = '星期获取出错';
+				W = 'Error';
+				break;
+			}
+		if(datestr == "" || datestr == undefined){
+			datestr = "Y/MM/DD hh:mm:ss";
+		}
+		//年 Year
+		if(datestr.indexOf("Y") != -1){
+			datestr = datestr.replace("Y", Y);
+		}
+		//月 Month
+		if(datestr.indexOf("M") != -1){
+			if(datestr.indexOf("MM") != -1){
+				datestr = datestr.replace("MM", addZero(M));
+			} else {
+				datestr = datestr.replace("M", M);
+			}
+		}
+		//日 day
+		if(datestr.indexOf("D") != -1){
+			if(datestr.indexOf("DD") != -1){
+				datestr = datestr.replace("DD", addZero(D));
+			} else {
+				datestr = datestr.replace("D", D);
+			}
+		}
+		//周、星期 week
+		if(datestr.indexOf("w") != -1){
+			datestr = datestr.replace("w", w);
+		}
+		if(datestr.indexOf("W") != -1){
+			datestr = datestr.replace("W",W);
+		}
+		//时 Hours
+		if(datestr.indexOf("h") != -1){
+			if(datestr.indexOf("hh") != -1){
+				datestr = datestr.replace("hh", addZero(h));
+			} else {
+				datestr = datestr.replace("h", addZero(h) > 12 ? h - 12 : h);
+			}
+		}
+		//分 minute
+		if(datestr.indexOf("m") != -1){
+			if(datestr.indexOf("mm") != -1){
+				datestr = datestr.replace("mm", addZero(m));
+			} else {
+				datestr = datestr.replace("m", m);
+			}
+		}
+		//秒 second
+		if(datestr.indexOf("s") != -1){
+			if(datestr.indexOf("ss") != -1){
+				datestr = datestr.replace("ss", addZero(s));
+			} else {
+				datestr = datestr.replace("s", s);
+			}
+		}
+		//对传进的值进行比对，如果小于10，则在其前面加入一个0；如输入3，返回结果为03。
+		function addZero(data){
+			if(data < 10){
+				data = "0" + data;
+			}
+			return data;
+		}
+		return datestr;
+	},
+	/**
+	 * get timestamp
+	 * 获取时间戳函数
+	 * @param {number} offset
+	 */
+	getTS:function(offset){
+		var TS = fdate.valueOf();
+		if(offset !== undefined){
+			if(typeof(offset) !== 'number'){
+				throw new Error("The parameter must be of type 'number'");
+			}
+			TS += 86400000*offset;
+			
+		}
+		return TS;
+	},
+	/**
+	 * timestamp to date
+	 * @param {number} ts // timestamp
+	 * @param {string} datestr
+	 */
+	tsToDate:function(ts,datestr){
+		let date;
+		if(ts !== undefined){
+			if(typeof(ts) !== "number"){
+				throw new Error("The parameter must be of type Numbers, you entered a "+typeof(ts)+" type.");
+			}
+			date = new Date(ts);
 		} else {
-			datestr = datestr.replace("M", M);
+			throw new Error('The parameter cannot be null');
 		}
-	}
-	if(datestr.indexOf("D") != -1){
-		if(datestr.indexOf("DD") != -1){
-			datestr = datestr.replace("DD", addZero(D));
-		} else {
-			datestr = datestr.replace("D", D);
+		if(datestr !== undefined){
+			if(typeof(datestr) !== 'string'){
+				throw new Error("The second parameter must be of type String");
+			}
+			date = this.getTime(datestr, date);
 		}
+		return date;
 	}
-	if(datestr.indexOf("w") != -1){
-		datestr = datestr.replace("w", w);
-	}
-	if(datestr.indexOf("W") != -1){
-		datestr = datestr.replace("W",W);
-	}
-	if(datestr.indexOf("h") != -1){
-		if(datestr.indexOf("hh") != -1){
-			datestr = datestr.replace("hh", addZero(h));
-		} else {
-			datestr = datestr.replace("h", addZero(h) > 12 ? h - 12 : h);
-		}
-	}
-	if(datestr.indexOf("m") != -1){
-		if(datestr.indexOf("mm") != -1){
-			datestr = datestr.replace("mm", addZero(m));
-		} else {
-			datestr = datestr.replace("m", m);
-		}
-	}
-	if(datestr.indexOf("s") != -1){
-		if(datestr.indexOf("ss") != -1){
-			datestr = datestr.replace("ss", addZero(s));
-		} else {
-			datestr = datestr.replace("s", s);
-		}
-	}
-	if(datestr.indexOf("TS" != -1)){
-		datestr = datestr.replace("TS",TS);
-	}
-	//对传进的值进行比对，如果小于10，则在其前面加入一个0；如输入3，返回结果为03。
-	function addZero(data){
-		if(data < 10){
-			data = "0" + data;
-		}
-		return data;
-	}
-	return datestr;
 }
 module.exports = fd;
